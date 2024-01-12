996dfdfcf1e7e31e12732bcfdde8d9864b8a860b
Christian Askeland <Christian.Askeland@carrier.com>
2024-01-12 15:55:13 +0100

    section 13.*

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+//! Module for searcing for patterns in text.
+//! 
+//! The module is a test project for learning Rust.
+//! 
+
 use std::fs;
 use std::env;
 use std::error::Error;
 
+/// Configuration data for the minigrep
+/// 
+/// 
+/// 
 pub struct Config {
     pub query: String,
     pub file_path: String,
@@ -9,13 +18,21 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(
+        mut args: impl Iterator<Item = String>
+    ) -> Result<Config, &'static str> {
+        args.next(); // skip app name
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config { query, file_path, ignore_case })
@@ -41,26 +58,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 
@@ -94,5 +103,5 @@ Trust me.";
             search_case_insensitive(query, contents)
         );
     }
-    
+
 }
